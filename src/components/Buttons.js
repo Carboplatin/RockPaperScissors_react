409517483b1b35c4ea872fe59bb3006bc6a44a9a
Cabroplatin <lisowski.mateusz90@gmail.com>
2020-01-29 18:41:59 +0000

    initial commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Buttons = ( )=> {
+    return (
+        <div>
+                <button>Test1</button>
+                <button>Test2</button>
+                <button>Test3</button>
+        </div>
+    )
+}
+
+export default Buttons
