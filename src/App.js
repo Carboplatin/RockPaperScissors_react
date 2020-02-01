2e29b22a130b6d312e5b999ad3e7de88f7611dba
Cabroplatin <lisowski.mateusz90@gmail.com>
2020-02-01 12:36:24 +0000

    second go at logic for comparison of results

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Buttons from './components/Buttons'
+import Output from './components/Output'
 import './App.css';
 
 
 class App extends Component {
+    state= {
+        results: [
+            {id: 1, content:'rock' },
+            {id: 1, content:'paper' },
+            {id: 1, content:'scissors' },
+        ]
+    }
+
+
+
     render ( ) {
         return (
             <div className="container">
-                <Buttons/>
+                <Buttons />
+                <Output results={this.state.results} />
             </div>
         )
     }
---
+++ src/components/Output.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Output = ({results})=> {
+    const compareResults = results
+
+    return (
+        <div>
+           <h1>Results</h1>     
+        </div>
+    )
+}
+
+
+
+export default Output
